4c69394c65d5c9c924909d8b5d009cfb365e7e1f
Willian Paixao <willian.paixaoo@gmail.com>
2014-07-25 02:11:15 -0400

    It's alive!
    Finally I can show something. Dashboard can now show real-time data from the
    clients.
    
    It retrieves the address from database and make some requests. Name and status
    are working like a charm.

--- views.py
+++ views.py
@@ -1,4 +1,11 @@
 from django.views.generic import TemplateView
+from ctrlpi_xbmc.models import Node
 
 class DashboardIndex(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardIndex, self).get_context_data(**kwargs)
+        n = Node.objects.all()
+        context['node'] = n
+        return context
