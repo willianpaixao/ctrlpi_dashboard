780b389ff40dd3448b9920b22db307ccc8fa327a
Willian Paixao <willian.paixaoo@gmail.com>
2014-07-30 01:03:55 -0400

    Node page added.
    ^/node/$ was added, along with the class, template, view and proper query.

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from dashboard.views import DashboardIndex
+from dashboard.views import DashboardNode
 
 urlpatterns = [
     url(r'^$', DashboardIndex.as_view()),
+    url(r'^(?P<id>\d+)/$', DashboardNode.as_view()),
 ]
--- views.py
+++ views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.views.generic import TemplateView
 from ctrlpi_xbmc.models import Node
 
@@ -7,5 +8,15 @@ class DashboardIndex(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(DashboardIndex, self).get_context_data(**kwargs)
         n = Node.objects.all()
-        context['node'] = n
+        context[u"node"] = n
         return context
+
+class DashboardNode(TemplateView):
+    template_name = "node.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardNode, self).get_context_data(**kwargs)
+        i = int(self.kwargs["id"])
+        context[u"node"] = Node.objects.get(pk=i)
+        return context
+
